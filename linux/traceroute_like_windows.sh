263d3cfd81ea14e6b96768ea37b2e63eaccecd99
honbey <git@honbey.com>
2021-05-04 00:46:33 +0800

    A minor modification

--- linux/traceroute_like_windows.sh
+++ linux/traceroute_like_windows.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-traceroute -n "$1" | cut -c 5-100 | awk '{printf "%-3s%-6s%-3s%-6s%-3s%-6s%-3s%-20s\n",FNR,$2,$3,$4,$5,$6,$7,$1}'
+traceroute -n "$1" | cut -c 5-100 | awk '{printf "%2s %8s %2s %8s %2s %8s %2s  %-20s\n",FNR,$2,$3,$4,$5,$6,$7,$1}' 

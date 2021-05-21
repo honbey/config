4e4dbc4a768d0c01426d0b81faf9776c736baa14
honbey <git@honbey.com>
2021-05-21 16:38:02 +0800

    Modify get file info by stat and format it as json

--- linux/file_json_by_stat.sh
+++ linux/file_json_by_stat.sh
@@ -8,7 +8,7 @@ c_path=$(pwd)
 
 for f in ${files}; do
   eval $(echo "i=($(stat -t ${f} | awk '{print $1" "$2" "$13}'))")
-  tmp="{\"filename\": \"${i[0]}\",\"update_time\": \"${i[2]}\",\"size\": \"${i[1]}\"},"
+  tmp="{\"type\":\"file\",\"filename\":\"${i[0]}\",\"update_time\":\"${i[2]}\",\"size\":\"${i[1]}\",\"remark\":\"via CDN\"},"
   content="${content}${tmp}"
   echo "[${content:0:-1}]" > "${c_path}/../$1.json"
 done

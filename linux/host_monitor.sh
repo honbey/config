6c3bad47e3364168e539970954c3b58e79503592
honbey <git@honbey.com>
2022-04-07 22:26:02 +0800

    perf(host monitor): better network and disk monitor.

--- linux/host_monitor.sh
+++ linux/host_monitor.sh
@@ -57,39 +57,54 @@ function disk_io {
 function net_flow {
   local bytes=($(awk -v eth=$1 '
     BEGIN {ORS=" "}
-    /'$1'/{print $'$2', $'$3'}
+    /'$1':/{print $'$2', $'$3'}
   ' /proc/net/dev))
   echo ${bytes[@]}
 }
 
-init_cpu_usage=($(cpu_usage))
-init_eno1_flow=($(net_flow eno1 2 10))
-init_eno1_package=($(net_flow eno1 3 11))
-init_vbox_flow=($(net_flow vboxnet0 2 10))
-init_vbox_package=($(net_flow vboxnet0 3 11))
-init_disk_in="$(disk_in)"
-init_disk_out="$(disk_out)"
-init_nvme_io=($(disk_io nvme0n1))
-init_sda_io=($(disk_io sda))
+# Host Monitor by infinite loop, you can also use crontab.
+interval=5
 
-#date '+%H:%M:%S.%N'
+# Interface configuration
+if1="eno1"
+if2="vboxnet0"
 
+# Disk configuration
+disk1="nvme0n1"
+disk2="sda"
+
+#
 total_mem="$(awk '/MemTotal/{total=$2} END {print total / 1024}' /proc/meminfo)"
 total_swap="$(awk '/SwapTotal/{total=$2} END {print total / 1024}' /proc/meminfo)"
-interval=5
+
+if [[ ${total_swap} == 0 ]]; then
+  total_swap=0.1
+fi
+
 ki=$(( interval * 1024 ))
 
+init_cpu_usage=($(cpu_usage))
+init_if1_flow=($(net_flow ${if1} 2 10))
+init_if1_package=($(net_flow ${if1} 3 11))
+init_if2_flow=($(net_flow ${if2} 2 10))
+init_if2_package=($(net_flow ${if2} 3 11))
+init_disk_in="$(disk_in)"
+init_disk_out="$(disk_out)"
+init_disk1_io=($(disk_io ${disk1}))
+init_disk2_io=($(disk_io ${disk2}))
+
 while true; do
+  # date '+%H:%M:%S.%N'
   sleep ${interval}
   curr_cpu_usage=($(cpu_usage))
-  curr_eno1_flow=($(net_flow eno1 2 10))
-  curr_eno1_package=($(net_flow eno1 3 11))
-  curr_vbox_flow=($(net_flow vboxnet0 2 10))
-  curr_vbox_package=($(net_flow vboxnet0 3 11))
+  curr_if1_flow=($(net_flow ${if1} 2 10))
+  curr_if1_package=($(net_flow ${if1} 3 11))
+  curr_if2_flow=($(net_flow ${if2} 2 10))
+  curr_if2_package=($(net_flow ${if2} 3 11))
   curr_disk_in="$(disk_in)"
   curr_disk_out="$(disk_out)"
-  curr_nvme_io=($(disk_io nvme0n1))
-  curr_sda_io=($(disk_io sda))
+  curr_disk1_io=($(disk_io ${disk1}))
+  curr_disk2_io=($(disk_io ${disk2}))
 
   curr_cpu_load=($(cpu_load))
   curr_app_mem="$(app_use_memory)"
@@ -100,49 +115,46 @@ while true; do
     awk '{print (($2 - $4) * 100) / ($1 - $3)}')"
 
   # [0] down, [1] up
-  eno1_io_speed=($(echo "${curr_eno1_flow[@]} ${init_eno1_flow[@]}" |
+  if1_io_speed=($(echo "${curr_if1_flow[@]} ${init_if1_flow[@]}" |
     awk '{print ($1 - $3) / '${ki}', ($2 - $4) / '${ki}'}'))
-  eno1_pkg_speed=($(echo "${curr_eno1_package[@]} ${init_eno1_package[@]}" |
+  if1_pkg_speed=($(echo "${curr_if1_package[@]} ${init_if1_package[@]}" |
     awk '{print ($1 - $3) / '${interval}', ($2 - $4) / '${interval}'}'))
-  vbox_io_speed=($(echo "${curr_vbox_flow[@]} ${init_vbox_flow[@]}" |
+  if2_io_speed=($(echo "${curr_if2_flow[@]} ${init_if2_flow[@]}" |
     awk '{print ($1 - $3) / '${ki}', ($2 - $4) / '${ki}'}'))
-  vbox_pkg_speed=($(echo "${curr_vbox_package[@]} ${init_vbox_package[@]}" |
+  if2_pkg_speed=($(echo "${curr_if2_package[@]} ${init_if2_package[@]}" |
     awk '{print ($1 - $3) / '${interval}', ($2 - $4) / '${interval}'}'))
 
   disk_i_speed="$(echo "${curr_disk_in} ${init_disk_in}" | awk '{printf "%.f", ($1 - $2) / '${interval}'}')"
   disk_o_speed="$(echo "${curr_disk_out} ${init_disk_out}" | awk '{printf "%.f", ($1 - $2) / '${interval}'}')"
 
   # [0] read, [1] write
-  nvme_speed=($(echo "${curr_nvme_io[@]} ${init_nvme_io[@]}" |
+  disk1_speed=($(echo "${curr_disk1_io[@]} ${init_disk1_io[@]}" |
     awk '{printf "%.f %.f", ($1 - $3) * 512 / '${ki}', ($2 - $4) * 512 / '${ki}'}'))
-  sda_speed=($(echo "${curr_sda_io[@]} ${init_sda_io[@]}" |
+  disk2_speed=($(echo "${curr_disk2_io[@]} ${init_disk2_io[@]}" |
     awk '{printf "%.f %.f", ($1 - $3) * 512 / '${ki}', ($2 - $4) * 512 / '${ki}'}'))
 
-  ms=$(date "+%N")
-  msec="$(date "+%s").${ms:0:3}"
-
-  s0="{\"msec\":${msec},\"time_local\":\"$(date '+%d/%b/%Y:%H:%M:%S +0800')\",\"cpu_usage_per\":${cpu_usage_per}"
+  s0="{\"timestamp\":\"$(date -u "+%Y-%m-%dT%H:%M:%S.%NZ")\",\"cpu_usage_per\":${cpu_usage_per}"
   s1=",\"cpu_load_1\":${curr_cpu_load[0]},\"cpu_load_5\":${curr_cpu_load[1]},\"cpu_load_15\":${curr_cpu_load[2]}"
-  s2=",\"eno1_in\":${eno1_io_speed[0]},\"eno1_out\":${eno1_io_speed[1]}"
-  s3=",\"eno1_pkg_in\":${eno1_pkg_speed[0]},\"eno1_pkg_out\":${eno1_pkg_speed[1]}"
-  s4=",\"vbox_in\":${vbox_io_speed[0]},\"vbox_out\":${vbox_io_speed[1]}"
-  s5=",\"vbox_pkg_in\":${vbox_pkg_speed[0]},\"vbox_pkg_out\":${vbox_pkg_speed[1]}"
-  s6=",\"app_mem\":${curr_app_mem},\"used_mem\":${curr_mem}"
-  s7=",\"total_mem\":${total_mem},\"used_swap\":${curr_swap},\"total_swap\":${total_swap}"
-  s8=",\"disk_out\":${disk_i_speed},\"disk_in\":${disk_o_speed}"
-  s9=",\"nvme_read\":${nvme_speed[0]},\"nvme_write\":${nvme_speed[1]}"
-  s10=",\"sda_read\":${sda_speed[0]},\"sda_write\":${sda_speed[1]}}"
+  s2=",\"app_mem\":${curr_app_mem},\"used_mem\":${curr_mem},\"total_mem\":${total_mem}"
+  s3=",\"used_swap\":${curr_swap},\"total_swap\":${total_swap}"
+  s4=",\"network\":{\"if1_in\":${if1_io_speed[0]},\"if1_out\":${if1_io_speed[1]}"
+  s5=",\"if1_pkg_in\":${if1_pkg_speed[0]},\"if1_pkg_out\":${if1_pkg_speed[1]}"
+  s6=",\"if2_in\":${if2_io_speed[0]},\"if2_out\":${if2_io_speed[1]}"
+  s7=",\"if2_pkg_in\":${if2_pkg_speed[0]},\"if2_pkg_out\":${if2_pkg_speed[1]}}"
+  s8=",\"disk\":{\"disk_out\":${disk_i_speed},\"disk_in\":${disk_o_speed}"
+  s9=",\"disk1_read\":${disk1_speed[0]},\"disk1_write\":${disk1_speed[1]}"
+  s10=",\"disk2_read\":${disk2_speed[0]},\"disk2_write\":${disk2_speed[1]}}}"
 
   echo "${s0}${s1}${s2}${s3}${s4}${s5}${s6}${s7}${s8}${s9}${s10}"
 
   init_cpu_usage=(${curr_cpu_usage[@]})
-  init_eno1_flow=(${curr_eno1_flow[@]})
-  init_eno1_package=(${curr_eno1_package[@]})
-  init_vbox_flow=(${curr_vbox_flow[@]})
-  init_vbox_package=(${curr_vbox_package[@]})
+  init_if1_flow=(${curr_if1_flow[@]})
+  init_if1_package=(${curr_if1_package[@]})
+  init_if2_flow=(${curr_if2_flow[@]})
+  init_if2_package=(${curr_if2_package[@]})
   init_disk_in="${curr_disk_in}"
   init_disk_out="${curr_disk_out}"
-  init_nvme_io=(${curr_nvme_io[@]})
-  init_sda_io=(${curr_sda_io[@]})
+  init_disk1_io=(${curr_disk1_io[@]})
+  init_disk2_io=(${curr_disk2_io[@]})
 done
 

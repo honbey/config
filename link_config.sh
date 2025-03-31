6d95a951eed773d03e0ccd8a079356fabeed8d90
honbey <honbey@qq.com>
2025-03-31 13:31:43 +0800

    perf(link_config): update shell script

--- link_config.sh
+++ link_config.sh
@@ -2,21 +2,28 @@
 
 set -x
 
-rm -rf ~/.zshrc ~/.gitconfig ~/.vimrc ~/.tmux.conf ~/.sqliterc ~/.npmrc ~/.env ~/.wezterm.lua
+rm -f ~/.zshrc ~/.gitconfig ~/.vimrc ~/.tmux.conf ~/.sqliterc ~/.npmrc \
+  ~/.env ~/.ssh/config
 
-rm -rf ~/.ssh/config ~/.config/nvim ~/.config/pip ~/.config/go
+rm -rf ~/.config/nvim ~/.config/pip ~/.config/go ~/.config/ghostty \
+  ~/.config/zed ~/.config/zsh
 
 CONFIG_DIR="$1"
 
-ln -s "${CONFIG_DIR}"/dotfile/.zshrc ~/.zshrc
-ln -s "${CONFIG_DIR}"/dotfile/.gitconfig ~/.gitconfig
-ln -s "${CONFIG_DIR}"/dotfile/.vimrc ~/.vimrc
-ln -s "${CONFIG_DIR}"/dotfile/.tmux.conf ~/.tmux.conf
-ln -s "${CONFIG_DIR}"/dotfile/.sqliterc ~/.sqliterc
-ln -s "${CONFIG_DIR}"/dotfile/.npmrc ~/.npmrc
-ln -s "${CONFIG_DIR}"/dotfile/.env ~/.env
-ln -s "${CONFIG_DIR}"/dotfile/.wezterm.lua ~/.wezterm.lua
-ln -s "${CONFIG_DIR}"/dotfile/ssh/config ~/.ssh/config
-ln -s "${CONFIG_DIR}"/dotfile/nvim ~/.config/nvim
-ln -s "${CONFIG_DIR}"/dotfile/pip ~/.config/pip
-ln -s "${CONFIG_DIR}"/dotfile/go ~/.config/go
+ln -s "${CONFIG_DIR}"/dotfile/tmux.conf ~/.tmux.conf
+ln -s "${CONFIG_DIR}"/dotfile/sqliterc ~/.sqliterc
+ln -s "${CONFIG_DIR}"/dotfile/npmrc ~/.npmrc
+ln -s "${CONFIG_DIR}"/dotfile/env ~/.env
+ln -s "${CONFIG_DIR}"/dotfile/ssh_config ~/.ssh/config
+
+ln -s "${CONFIG_DIR}"/zsh/zshrc ~/.zshrc
+ln -s "${CONFIG_DIR}"/zsh ~/.config/zsh
+
+ln -s "${CONFIG_DIR}"/git/gitconfig ~/.gitconfig
+ln -s "${CONFIG_DIR}"/vim/vimrc ~/.vimrc
+
+ln -s "${CONFIG_DIR}"/nvim ~/.config/nvim
+ln -s "${CONFIG_DIR}"/pip ~/.config/pip
+ln -s "${CONFIG_DIR}"/go ~/.config/go
+ln -s "${CONFIG_DIR}"/ghostyy ~/.config/ghostty
+ln -s "${CONFIG_DIR}"/zed ~/.config/zed

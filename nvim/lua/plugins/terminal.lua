661594b86408593ebf0a5a6ae1cdf5656873451d
honbey <honbey@qq.com>
2025-05-08 18:37:45 +0800

    update nvim config(terminal)

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -2,15 +2,14 @@ return {
   {
     "akinsho/toggleterm.nvim",
     opts = {
-      open_mapping = "<c-`>",
+      open_mapping = "<C-/>",
     },
     -- stylua: ignore
     keys = {
       { "<leader>ft", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Toggle Terminal" },
       { "<leader>tt", "<cmd>lua require('toggleterm.terminal').Terminal:new():toggle()<cr>", desc = "Terminal" },
-      { "<C-`>", "<cmd>ToggleTerm<cr>", mode = { "n" } },
-      { "<C-`>", "<cmd>close<cr>", mode = { "t" } },
-      { "<C-[>", "<C-\\><C-n>", mode = { "t" } },
+      { "<C-_>", "<cmd>ToggleTerm<cr>", mode = { "n" } },
+      { "<C-_>", "<cmd>close<cr>", mode = { "t" } },
     },
   },
 }
